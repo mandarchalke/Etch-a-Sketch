84649f76599db7dbad4de67a9b17785fb0c1c80f
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-15 12:59:01 +0530

    Added a function which creates column.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-function createGrid(times) {
+function createRow(times) {
     for (let i = 0; i < times; i++) {
         const container = document.getElementById('main');
         const div = document.createElement('div');
-        div.classList.add('grid');
+        div.classList.add('row');
         container.appendChild(div);
     }
 }
 
-const input = prompt("Number: ")
-createGrid(input);
+function createColumn(times) {
+    for(let i = 0; i < times; i++) {
+        const container = document.getElementById('main');
+        const div = document.createElement('div');
+        div.classList.add('column');
+        container.appendChild(div);
+    }
+}
+
+
+const input1 = prompt("Number 1: ");
+const input2 = prompt("Number 2: ");
+
+createRow(input1);
+createColumn(input2);
