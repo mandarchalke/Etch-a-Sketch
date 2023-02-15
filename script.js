363e194f50779164498155fba6c876f7f6926cd8
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-15 12:30:05 +0530

    Added createGrid function. * This function take a value and creates that many new divs.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const container = document.getElementById('main');
-const div = document.createElement('div');
-div.classList.add('grid');
-container.appendChild(div);
+function createGrid(times) {
+    for (let i = 0; i < times; i++) {
+        const container = document.getElementById('main');
+        const div = document.createElement('div');
+        div.classList.add('grid');
+        container.appendChild(div);
+    }
+}
+
+const input = prompt("Number: ")
+createGrid(input);
