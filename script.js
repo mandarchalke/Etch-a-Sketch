92ee0270aacc47d9b0717b6859d4e8f1ae9c74f9
chalkemandar71 <chalkemandar71@gmail.com>
2023-02-20 12:26:25 +0530

    Removed column function.

--- script.js
+++ script.js
@@ -7,18 +7,6 @@ function createRow(times) {
     }
 }
 
-function createColumn(times) {
-    for(let i = 0; i < times; i++) {
-        const container = document.getElementById('main');
-        const div = document.createElement('div');
-        div.classList.add('column');
-        container.appendChild(div);
-    }
-}
-
-
 const input1 = prompt("Number 1: ");
-const input2 = prompt("Number 2: ");
 
 createRow(input1);
-createColumn(input2);
